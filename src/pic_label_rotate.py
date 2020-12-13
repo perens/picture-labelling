aaea31528e0a311b0a12dc574e9e1da0799ab1a0
perens <sanderperens@gmail.com>
2020-12-13 13:57:28 +0200

    requirements, init script

---
+++ src/pic_label_rotate.py
@@ -0,0 +1,59 @@
+import configparser
+import torchvision.transforms as tfms
+from fastai.vision import *
+
+config = configparser.ConfigParser()
+config.read('config/conf.ini')
+
+model = load_learner(config['CONF']['model_folder'], config['CONF']['model_name'])
+
+def get_rotation_degree(input_string):
+    return int(''.join(filter(str.isdigit, input_string)))
+    
+def get_main_classification(input_string):
+    return ''.join(filter(str.isalpha, str(input_string)))
+
+def pil2fast(img):
+    return vision.image.Image(pil2tensor(img, dtype=np.float32).div_(255))
+
+def fast2pil(img):
+    return tfms.ToPILImage()(img.data).convert("RGB")
+
+
+
+# TODO properties existance, validation..
+input_folder = config['CONF']['input_folder']
+output_folder = config['CONF']['output_folder']
+
+for i in os.listdir(input_folder):
+    if i.endswith(".jpg"): # TODO png?
+        img_fastai = open_image(os.path.join(input_folder, i))
+        img_title, img_extension = os.path.splitext(i)
+        pred_class, pred_idx, outputs = model.predict(img_fastai)
+        prediction = (str(pred_class), round(max(outputs.numpy()) * 100))
+        
+        # 90 or 270 degrees, rotate and predict again
+        rotate_degree = get_rotation_degree(str(pred_class))
+        if rotate_degree in [90, 270]:
+            img_pil = fast2pil(img_fastai)
+            img_pil = img_pil.rotate(rotate_degree, expand=True)
+            img_fastai = pil2fast(img_pil)
+    
+            pred_class, pred_idx, outputs = model.predict(img_fastai)
+            prediction = (str(pred_class), round(max(outputs.numpy()) * 100))
+            rotate_degree = get_rotation_degree(str(pred_class))
+            
+        # continue with rotation
+        img_pil = fast2pil(img_fastai)
+        img_pil = img_pil.rotate(360 - rotate_degree, expand=True)
+        img_fastai = pil2fast(img_pil)
+        
+        path_to_class = output_folder + "/" + get_main_classification(str(pred_class))
+        
+        # TODO find all categories from model prehand..
+        if not os.path.exists(path_to_class):
+            os.makedirs(path_to_class)
+        
+        new_img_title = img_title + "_" + prediction[0] + "_" + str(prediction[1]) + img_extension
+        new_img_path = os.path.join(path_to_class, new_img_title)
+        img_fastai.save(new_img_path)
